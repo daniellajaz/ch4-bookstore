7f6c8f4b31ebb3a2c7250250427568bdecea8f51
daniellajaz <danielithatoroo@gmail.com>
2024-01-10 17:25:44 -0500

    Agregue el capitulo 5

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local
     "accounts.apps.AccountsConfig", # new
+    "pages.apps.PagesConfig", # new
 ]
 AUTH_USER_MODEL = "accounts.CustomUser" # new
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"], # new,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
